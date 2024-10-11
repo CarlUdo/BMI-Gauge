9a979f8090954b8794e2712bbad1bc7ad4f65e16
CarlUdo <carl@wbsweden.com>
2024-10-11 21:01:19 +0200

    Set up a basic server, receive a request and send a response

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hej');
+import http from 'http';
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const server = http.createServer((req, res) => {
+  console.log(`Getting a ${req.method} request`);
+
+  res.writeHead(200, {
+    "Content-type": "appllication/json"
+  });
+
+  res.end(JSON.stringify({ message: "Hello from server!"}));
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server is listening on http://${HOST}:${PORT}`);
+});
