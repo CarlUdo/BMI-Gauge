4accbc11ea00cfd15b94d6eace777edde595392f
CarlUdo <carl@wbsweden.com>
2024-10-12 20:00:00 +0200

    Make custom error class

---
+++ backend/src/Errors.ts
@@ -0,0 +1,23 @@
+export class CustomError extends Error {
+  cause?: Error;  
+
+  constructor(message: string, cause?: Error) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+    if (cause) {
+      this.cause = cause;
+    }
+    this.name = this.constructor.name;
+  }
+
+  get statusCode() {
+    if (this.name === 'BadRequestError') return 400;
+    return 500;
+  }
+}
+
+export class BadRequestError extends CustomError {
+  constructor(message: string, cause?: Error) {
+    super(message, cause);
+  }
+};
