3378f19dedbb68a2c4a40416089f5c1bafb54bf7
CarlUdo <carl@wbsweden.com>
2024-10-12 17:50:12 +0200

    Refactor code and make server call itsel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,10 @@
-import http from "http";
-import type { BmiRequest } from "./types";
-import { getBmiResponse } from "./get-bmi-response";
+import server from './api';
 
-const PORT = 3000;
-const HOST = "0.0.0.0";
+(() => {
+  const PORT = 3000;
+  const HOST = "0.0.0.0";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:8080",
-    "Access-Control-Allow-Headers": "Content-Type, Accept",
-  });
-
-  if (req.method !== "POST") {
-    return res.end(JSON.stringify({ error: `Invalid method: ${req.method}` }));
-  }
-
-  let body = "";
-
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
-
-  req.on("end", () => {
-    const bmiResponse = getBmiResponse(JSON.parse(body) as BmiRequest);
-
-    res.end(JSON.stringify(bmiResponse));
-  });
-});
-
-server.listen(PORT, HOST, () => {
-  console.log(`Server is listening on http://${HOST}:${PORT}`);
-});
+  server.listen(PORT, HOST, () => {
+    console.log(`Server is listening on http://${HOST}:${PORT}`);
+  });    
+})();
