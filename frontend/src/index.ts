2d7aae416cf2853553206c1b44b814abc54f45a6
CarlUdo <carl@wbsweden.com>
2024-10-12 10:33:40 +0200

    Make an eventlistener and a function for posting requests

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,1 +1,33 @@
-console.log('Hej');
+const submitButton = document.getElementById("submitButton") as HTMLButtonElement;
+const heightField = document.getElementById("heightField") as HTMLInputElement;
+const weightField = document.getElementById("heightField") as HTMLInputElement;
+const baseUrl = 'http://localhost:3000';
+
+type BmiRequest = {
+  height: string;
+  weight: string;
+};
+
+const postDataToApi = async (bmiRequestObj: BmiRequest) => {
+  const response = await fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(bmiRequestObj)
+  });
+
+  const serverData = await response.json();
+};
+
+submitButton.addEventListener('click', event => {
+  event.preventDefault();
+
+  const bmiRequestObj = {
+    height: heightField.value,
+    weight: weightField.value
+  };
+
+  postDataToApi(bmiRequestObj); 
+});
