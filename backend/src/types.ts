784de789f28705e40d48a94412aae3c72650da88
CarlUdo <carl@wbsweden.com>
2024-10-12 15:43:06 +0200

    Make a BmiAdvice type

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -2,3 +2,11 @@ export type BmiRequest = {
   height: string;
   weight: string;
 };
+
+export type BmiAdvice = {
+  underweight: string;
+  healthyRange: string;
+  overweight: string;
+  obesity: string;
+  severeObesity: string;
+};
