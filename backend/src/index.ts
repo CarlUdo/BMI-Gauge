f65e071229c1b9a38b0773057aee99adbcf09bd4
CarlUdo <carl@wbsweden.com>
2024-10-12 15:44:19 +0200

    Update call to getRmiResponse function and run prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,32 @@
-import http from 'http';
-import type { BmiRequest } from './types';
+import http from "http";
+import type { BmiRequest } from "./types";
+import { getBmiResponse } from "./get-bmi-response";
 
 const PORT = 3000;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     "Content-Type": "application/json",
-    'Access-Control-Allow-Origin': 'http://localhost:8080',
-    'Access-Control-Allow-Headers': 'Content-Type, Accept'
+    "Access-Control-Allow-Origin": "http://localhost:8080",
+    "Access-Control-Allow-Headers": "Content-Type, Accept",
   });
 
-  if (req.method !== 'POST') {
-    return res.end(JSON.stringify({ error: `Invalid method: ${req.method}`}));
+  if (req.method !== "POST") {
+    return res.end(JSON.stringify({ error: `Invalid method: ${req.method}` }));
   }
 
-  let body = '';
+  let body = "";
 
-  req.on('data', chunk => {
+  req.on("data", (chunk) => {
     body += chunk;
   });
 
-  req.on('end', () => {
-    const bmiRequest = JSON.parse(body) as BmiRequest;
+  req.on("end", () => {
+    const bmiResponse = getBmiResponse(JSON.parse(body) as BmiRequest);
 
-    res.end(JSON.stringify({ message: `Hello ${bmiRequest.height}`}));
-  });  
+    res.end(JSON.stringify(bmiResponse));
+  });
 });
 
 server.listen(PORT, HOST, () => {
