6e12e33c5a118b3a80ea84754af9a489c36504c9
CarlUdo <carl@wbsweden.com>
2024-10-12 11:26:34 +0200

    Update script file to render data

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,6 +1,7 @@
 const submitButton = document.getElementById("submitButton") as HTMLButtonElement;
 const heightField = document.getElementById("heightField") as HTMLInputElement;
-const weightField = document.getElementById("heightField") as HTMLInputElement;
+const weightField = document.getElementById("weightField") as HTMLInputElement;
+const outputArea = document.getElementById("outputArea") as HTMLInputElement;
 const baseUrl = 'http://localhost:3000';
 
 type BmiRequest = {
@@ -8,26 +9,37 @@ type BmiRequest = {
   weight: string;
 };
 
-const postDataToApi = async (bmiRequestObj: BmiRequest) => {
+type BmiResponse = {
+  height: string;
+  weight: string;
+};
+
+const renderData = (data: BmiResponse) => {
+  outputArea.innerHTML = data.height;
+};
+
+const postDataToApi = async (bmiRequest: BmiRequest) => {
   const response = await fetch(baseUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
-    body: JSON.stringify(bmiRequestObj)
+    body: JSON.stringify(bmiRequest)
   });
 
-  const serverData = await response.json();
+  const serverData = await response.json() as BmiResponse;
+
+  renderData(serverData);
 };
 
 submitButton.addEventListener('click', event => {
   event.preventDefault();
 
-  const bmiRequestObj = {
+  const bmiRequest = {
     height: heightField.value,
     weight: weightField.value
   };
 
-  postDataToApi(bmiRequestObj); 
+  postDataToApi(bmiRequest); 
 });
