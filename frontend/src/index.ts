ff782f5fce19aa6ed4e6947f5da7ccb6d4e1f502
CarlUdo <carl@wbsweden.com>
2024-10-12 14:25:16 +0200

    Write functions and event listeners

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,9 +1,16 @@
+/**
+ *  Please note that in a real world project the following functions would be divided
+ *  into different files/modules and bundled together at build time.
+ *  For the convenience of this assignment all functions are collected into one script file.
+ */
+
 const submitButton = document.getElementById("submitButton") as HTMLButtonElement;
 const heightField = document.getElementById("heightField") as HTMLInputElement;
 const weightField = document.getElementById("weightField") as HTMLInputElement;
 const outputArea = document.getElementById("outputArea") as HTMLInputElement;
 const baseUrl = 'http://localhost:3000';
 
+/** Types */ 
 type BmiRequest = {
   height: string;
   weight: string;
@@ -14,11 +21,28 @@ type BmiResponse = {
   weight: string;
 };
 
-const renderData = (data: BmiResponse) => {
-  outputArea.innerHTML = data.height;
+
+/** Helper functions */
+const isInteger = (str: string) => {
+  return /^-?\d+$/.test(str);
 };
 
-const postDataToApi = async (bmiRequest: BmiRequest) => {
+const isValidIntegers = (heightInput: string, weightInput: string) => {
+  if (!isInteger(heightInput) || !isInteger(weightInput)) return false;
+
+  const isValidHeight = Number(heightInput) > 80 && Number(heightInput) < 280;
+  const isValidWeight = Number(weightInput) > 20 && Number(weightInput) < 700;
+
+  return isValidHeight && isValidWeight;
+};
+
+
+/** Post and render functions */
+const renderData = (bmiResponse: BmiResponse) => {
+  outputArea.innerHTML = JSON.stringify(bmiResponse);
+};
+
+const makePostRequest = async (bmiRequest: BmiRequest) => {
   const response = await fetch(baseUrl, {
     method: "POST",
     headers: {
@@ -28,18 +52,49 @@ const postDataToApi = async (bmiRequest: BmiRequest) => {
     body: JSON.stringify(bmiRequest)
   });
 
-  const serverData = await response.json() as BmiResponse;
-
-  renderData(serverData);
+  return await response.json() as BmiResponse;
 };
 
-submitButton.addEventListener('click', event => {
-  event.preventDefault();
-
-  const bmiRequest = {
+const postDataToApi = async () => {
+  const bmiRequest: BmiRequest = {
     height: heightField.value,
     weight: weightField.value
   };
 
-  postDataToApi(bmiRequest); 
+  const bmiResponse = await makePostRequest(bmiRequest);
+
+  renderData(bmiResponse);
+};
+
+
+/** Event listeners */
+const initListeners = () => {
+  submitButton.addEventListener('click', e => {
+    e.preventDefault();
+    postDataToApi(); 
+    heightField.value = '';
+    weightField.value = '';
+    outputArea.value = '';
+    submitButton.setAttribute('disabled', 'true');
+  });
+  
+  heightField.addEventListener('change', () => {
+    submitButton.setAttribute('disabled', 'true');
+  
+    if (isValidIntegers(heightField.value, weightField.value)) {
+      submitButton.removeAttribute('disabled');
+    }
+  });
+  
+  weightField.addEventListener('change', () => {  
+    submitButton.setAttribute('disabled', 'true');
+  
+    if (isValidIntegers(heightField.value, weightField.value)) {
+      submitButton.removeAttribute('disabled');
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  initListeners();
 });
