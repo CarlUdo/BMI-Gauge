ba62fc882488000409c1649009a8cc08e7f821bd
CarlUdo <carl@wbsweden.com>
2024-10-13 07:14:11 +0200

    Update api

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,11 +1,9 @@
 import http from "http";
 import type { BmiRequest } from "./types";
 import { getBmiResponse } from "./get-bmi-response";
-import { validateBmiRequest } from "./validate-bmi-request";
 import { BadRequestError } from "./Errors";
 
 export default http.createServer((req, res) => {
-  try {
     res.writeHead(200, {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "http://localhost:8080",
@@ -23,21 +21,22 @@ export default http.createServer((req, res) => {
     });
   
     req.on("end", () => {
-      const bmiRequest = JSON.parse(body) as BmiRequest;
-      validateBmiRequest(bmiRequest);
-      const bmiResponse = getBmiResponse(bmiRequest);
-  
-      res.end(JSON.stringify(bmiResponse));
+      try {
+        const bmiRequest = JSON.parse(body) as BmiRequest;
+
+        const bmiResponse = getBmiResponse(bmiRequest);
+    
+        res.end(JSON.stringify(bmiResponse));
+      } catch (error) {
+        if (error instanceof BadRequestError) {
+          res.statusCode = error.statusCode;
+          res.end(JSON.stringify({ error: error.message}));
+          return;
+        }
+        res.statusCode = 500;
+        res.end(JSON.stringify({ error: "500 INTERNAL_SERVER_ERROR"}));
+        return;
+      }      
     });
-  } catch (error) {    
-    res.setHeader('Content-Type', 'application/json');
-    if (error instanceof BadRequestError) {
-      res.statusCode = error.statusCode;
-      res.end(JSON.stringify({ error: error.message}));
-      return error.message;
-    }
-    res.statusCode = 500;
-    res.end(JSON.stringify({ error: "500 INTERNAL_SERVER_ERROR"}));
-    return '500 INTERNAL_SERVER_ERROR';
-  }  
+   
 });
