0c85ec5f6618f8c5aed334cdcf24d6e80b474a2e
CarlUdo <carl@wbsweden.com>
2024-10-13 19:50:47 +0200

    Refactor strings

--- backend/src/get-advice.ts
+++ backend/src/get-advice.ts
@@ -1,15 +1,32 @@
 import { getEmoji } from "./get-emoji";
 import type { Advice } from "./types";
 
-const underweight = `You're struggling with underweight. Gaining weight can be a healthy and gradual process. Seeking guidance from a nutritionist or dietitian can help you develop a balanced plan to reach a healthier weight. Take care of yourself!`;
+const underweight =
+  `You're struggling with underweight. Gaining weight can be a healthy and gradual process.` +
+  `Seeking guidance from a nutritionist or dietitian can help you develop a balanced plan to reach a healthier weight.` +
+  `Take care of yourself!`;
 
-const healthyRange = `You're having a healthy weight. Maintaining a healthy weight is fantastic. Keep focusing on balanced nutrition, regular exercise, and staying mindful of your well-being. You’re doing great!`;
+const healthyRange =
+  `You're having a healthy weight. Maintaining a healthy weight is fantastic.` +
+  `Keep focusing on balanced nutrition, regular exercise, and staying mindful of your well-being.` +
+  `You’re doing great!`;
 
-const overweight = `You are struggling with overweight. It’s great that you’re thinking about your health. Working with a healthcare professional can help you develop a personalized plan to achieve a healthier weight. Focus on balanced nutrition, regular physical activity, and self-care. You've got this!`;
+const overweight =
+  `You are struggling with overweight. It’s great that you’re thinking about your health.` +
+  `Working with a healthcare professional can help you develop a personalized plan to achieve a healthier weight.` +
+  `Focus on balanced nutrition, regular physical activity, and self-care. You've got this!`;
 
-const obesity = `You´re struggling with obesity. Focusing on your health is commendable. Working closely with a healthcare provider can help create a sustainable plan to achieve a healthier weight. Remember to be patient with yourself, prioritize balanced nutrition and regular physical activity, and take things one step at a time. You're on the right path!`;
+const obesity =
+  `You´re struggling with obesity. Focusing on your health is commendable.` +
+  `Working closely with a healthcare provider can help create a sustainable plan to achieve a healthier weight.` +
+  `Remember to be patient with yourself, prioritize balanced nutrition and regular physical activity.` +
+  `Take things one step at a time. You're on the right path!`;
 
-const severeObesity = `You´re struggling with severe obesity. Severe obesity poses significant health risks and needs urgent attention. It increases the likelihood of serious conditions like heart disease, diabetes, and hypertension. It's crucial to seek help from a healthcare professional to develop a plan that addresses these risks and improves overall health. Your well-being is a priority; take action now.`;
+const severeObesity =
+  `You´re struggling with severe obesity. Severe obesity poses significant health risks.` +
+  `It needs urgent attention. It increases the likelihood of serious conditions like heart disease, diabetes, and hypertension.` +
+  `It's crucial to seek help from a healthcare professional to develop a plan that addresses these risks and improves overall health.` +
+  `Your well-being is a priority; take action now.`;
 
 const advice: Advice = {
   underweight,
