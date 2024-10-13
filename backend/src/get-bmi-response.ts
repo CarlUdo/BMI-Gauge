6de6879bc36c96f7f01299c46abae07eea2d34bb
CarlUdo <carl@wbsweden.com>
2024-10-13 07:13:47 +0200

    Update function

--- backend/src/get-bmi-response.ts
+++ backend/src/get-bmi-response.ts
@@ -1,8 +1,11 @@
 import { calculateBmi } from "./calculate-bmi";
 import { getAdvice } from "./get-advice";
 import type { BmiRequest } from "./types";
+import { validateBmiRequest } from "./validate-bmi-request";
 
 export const getBmiResponse = (bmiRequest: BmiRequest) => {
+  validateBmiRequest(bmiRequest);
+  
   const bmi = calculateBmi(bmiRequest);
   const advice = getAdvice(bmi);
 
